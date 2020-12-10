6d37454a19a4afe57829fb5b43e47f74cdb5b411
maxjacobzander <maxjacobzander@gmail.com>
2020-12-10 10:25:01 -0600

    defined mocked function for playInterval and moved script tags to bottom of html file

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
-const soundButton = document.getElementById('sound')
+const soundButton = document.getElementById("sound")
 
-const nextButton = document.getElementById('next')
+const nextButton = document.getElementById('next');
 
-const option1 = document.getElementById('interval1')
+const option1 = document.getElementById('interval1');
 
-const option2 = document.getElementById('interval2')
+const option2 = document.getElementById('interval2');
 
-const option3 = document.getElementById('interval3')
+const option3 = document.getElementById('interval3');
 
-const option4 = document.getElementById('interval4')
+const option4 = document.getElementById('interval4');
 
 function playInterval() {
-    soundButton.addEventListener("click", )
+    console.log("I'm playing an interval!")
 }
+
+soundButton.addEventListener('click', playInterval);
 
 function populateAnswers() {
     
