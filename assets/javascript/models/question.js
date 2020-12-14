8e4225c4eb10ad89c4c47d09dacd664f951a5354
maxjacobzander <maxjacobzander@gmail.com>
2020-12-13 22:38:13 -0600

    added question constructor

---
+++ assets/javascript/models/question.js
@@ -0,0 +1,12 @@
+class Question {
+    constructor(interval, answer_1, answer_2, answer_3, answer_4, correct_answer, game_id){
+        this.interval = interval
+        this.answer_1 = answer_1
+        this.answer_2 = answer_2
+        this.answer_3 = answer_3
+        this.answer_4 = answer_4
+        this.correct_answer = correct_answer
+        this.game_id = game_id
+    }
+
+}
