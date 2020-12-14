63834a56c47fdeb151fd525db67575571e31129f
maxjacobzander <maxjacobzander@gmail.com>
2020-12-13 22:16:14 -0600

    built out static addIntervals

--- assets/javascript/services/API.js
+++ assets/javascript/services/API.js
@@ -1,7 +1,11 @@
 class API {
     static addIntervals (){
-        fetch("http://localhost:3000/questions")
+        fetch("http://localhost:3000/games")
         .then(resp => resp.json())
-        .then(console.log)
+        .then(game => {
+            game.forEach(game => {
+                const{id, score} = game
+            })
+        } )
     }
 }
